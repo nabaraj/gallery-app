9c8d2a2726659db32c95dfd70617972724ee2437
Nabaraj Saha <nabara2k@gmail.com>
2021-01-25 00:24:58 +0530

    change action

--- src/view/album/album.action.js
+++ src/view/album/album.action.js
@@ -2,15 +2,10 @@ import requestApi from './../../utils/requestApi';
 import { GET_ALBUMS } from './../../utils/constants';
 export function getAlbums() {
   return function (dispatch) {
-    return new Promise((resolve, reject) => {
-      requestApi.get("https://jsonplaceholder.typicode.com/albums").then(res => {
-        dispatch({ type: GET_ALBUMS, data: res.data })
-        resolve(res.data);
-      }).catch(err => {
-        console.log(err);
-        reject(err);
-      })
-    })
+    requestApi.get("https://jsonplaceholder.typicode.com/albums").then(res => {
+      dispatch({ type: GET_ALBUMS, data: res.data });
+    }).catch(err => {
+      console.log(err);
+    });
   }
-
 };
